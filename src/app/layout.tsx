40c0ab475466ee28738c24dd61174c9d4339d380
Cole Gawin <colegawin@gmail.com>
2024-11-01 20:17:45 -0700

    chore: make app installable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import "./index.css";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body>{children}</body>
     </html>
   );
